ddc04df6ad2bd4fbd7ac36cf2d27f3c55ea19219
Matheus Alves Pereira <matttalves@gmail.com>
2022-10-18 18:40:18 -0300

    refactor: better logging info on start

--- src/services/s3/buckets.ts
+++ src/services/s3/buckets.ts
@@ -11,9 +11,16 @@ export function listS3Buckets() {
 }
 
 function checkS3Bucket() {
+  console.log('Checking S3 Bucket')
+
   return new Promise((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      console.log('Taking too long? Consider check `S3_BASE_URL` is right')
+    }, 5000)
+
     s3.headBucket(BUCKET_PARAMS, (err, data) => {
       if (err) reject(err)
+      clearTimeout(timeout)
       resolve(data)
     })
   })
@@ -38,6 +45,8 @@ export async function initializeS3Bucket(callback: () => void) {
     if (awsError.statusCode === 404) {
       console.log(`\nBucket '${BUCKET_PARAMS.Bucket}' not found, creating...`)
       createS3Bucket().then(callback).catch(console.error)
+    } else {
+      console.log('Something wrong:', error)
     }
   }
 }
